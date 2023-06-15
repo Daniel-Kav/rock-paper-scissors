4a1c09831f6653d265c98e2147c1718a189f3f9f
daniel-kav <dankavatha01@gmail.com>
2023-06-15 16:25:28 +0300

    compare two player choices and display result to user

--- paper.js
+++ paper.js
@@ -5,3 +5,15 @@ function getComputerChoice() {
     var randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
   }
+//compare the two players' choices and display result to user
+function compareChoices(playerOne, playerTwo) {
+  if (playerOne === "Rock" && playerTwo === "Scissors") {
+    alert("You win!");
+    } else if (playerOne === "Paper" && playerTwo === "Rocks") {
+      alert("You lose");
+      } else if (playerOne === "Scissors" && playerTwo === "Paper"){
+        alert("You Win!")
+        }else{
+          alert ("It's a tie.")
+          };
+          //reset game button
