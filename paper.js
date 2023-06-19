adbe8d929b789bca953079c502e6c2040d4d771c
daniel-kav <dankavatha01@gmail.com>
2023-06-19 19:34:27 +0300

    playround added

--- paper.js
+++ paper.js
@@ -1,11 +1,31 @@
 
 //get computer choice
 function getComputerChoice() {
-    var choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['Rock', 'Paper', 'Scissors'];
     var randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
   }
 
+function checkwinner(playerselection , computerselection){
+  if (computerselection === playerselection) {
+    console.log('tie');
+  }
+  else if{
+    //check winner
+    (playerselection == "Rock" && computerselection == "Scissors") ||
+    (playerselection == "Paper" && computerselection == "Rock") ||
+    (playerselection == "Scissors" && computerselection == "Paper");
+    return "Player"
+  }
+  else{
+    return "Computer"
+  }
+
+}
+
+function playround\(playerselection ,computerselection){
+  const result = checkwinner(playerselection,computerselection)
+}
 
 console.log(getComputerChoice())
 console.log ('run')
